436cc480895c140f10385c3ca51b6d7a58c74afd
Christian Lisangola <jochri3@gmail.com>
2023-02-10 13:52:40 +0300

    13. Add secret env values

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ const { clientsRouter } = require('./clients/clients.routes')
 const { ordersRouter } = require('./orders/orders.routes')
 const { Pool } = require('pg')
 
+require('dotenv').config()
+
 const app = express()
 app.use('/clients', clientsRouter)
 app.use('/orders', ordersRouter)
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'crm-backend-poe-front-28-nov-2022',
-  user: 'christianlisangola',
-  password: '1234',
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 })
 
 const PORT = 3000
